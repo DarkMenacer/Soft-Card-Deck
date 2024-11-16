6922a441afc4c4aecc5a26d6ea128d990cf9fec6
Pranav Chatur <pranavchatur@gmail.com>
2024-11-16 16:45:40 +0530

    Add Board View, Compact Deck view and Fan Deck view

--- src/models/Board/Board.h
+++ src/models/Board/Board.h
@@ -5,9 +5,9 @@
 namespace SoftCardDeck {
 	class BoardModel {
 		private:
-			SoftCardDeck::DeckModel __facedUpDeck, __facedDownDeck, __removedDeck;
+			SoftCardDeck::DeckModel *__facedUpDeck, *__facedDownDeck, *__removedDeck;
 		public:
-			BoardModel(SoftCardDeck::DeckModel _facedUpDeck, SoftCardDeck::DeckModel _facedDownDeck, SoftCardDeck::DeckModel _removedDeck);
+			BoardModel(SoftCardDeck::DeckModel *_facedUpDeck, SoftCardDeck::DeckModel *_facedDownDeck, SoftCardDeck::DeckModel *_removedDeck);
 			SoftCardDeck::DeckModel *faced_up_deck();
 			SoftCardDeck::DeckModel *faced_down_deck();
 			SoftCardDeck::DeckModel *removed_deck();
--- src/models/Deck/Deck.h
+++ src/models/Deck/Deck.h
@@ -11,12 +11,11 @@ namespace SoftCardDeck {
 			int __deckSize;
 			void add_card(SoftCardDeck::CardModel card);
 			void delete_current_card();
-
 		public:
 			explicit DeckModel(std::deque<CardModel>);
 			SoftCardDeck::CardModel top_card();
 			SoftCardDeck::CardModel bottom_card();
-			SoftCardDeck::CardModel current_card();
+			SoftCardDeck::CardModel &current_card();
 			void next_card();
 			void previous_card();
 			void flip_current_card();
@@ -26,7 +25,6 @@ namespace SoftCardDeck {
 			void sort();
 			int size();
 			void move_current_card_to(DeckModel &d);
-			std::deque<SoftCardDeck::CardModel> get_cards_copy();
+			std::deque<SoftCardDeck::CardModel> *deckCards();
 	};
-
 }
